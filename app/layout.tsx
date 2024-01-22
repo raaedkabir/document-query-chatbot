27608ddd8ab080b5e66a0b700952ccc79e29c0bf
Raaed Kabir <raaed.kabir@gmail.com>
2024-01-22 14:21:17 -0500

    Update Tailwind CSS configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Montserrat, Nunito } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+})
+const nunito = Nunito({
+  subsets: ['latin'],
+  variable: '--font-nunito',
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +22,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${montserrat.variable} ${nunito.variable}`}>
+      <body className="min-h-screen bg-gray-light">{children}</body>
     </html>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 const config: Config = {
   content: [
@@ -7,7 +8,25 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#FFFFFF',
+      black: '#000000',
+      gray: {
+        light: '#E9E8EE',
+        DEFAULT: '#99989F',
+        dark: '#575151',
+      },
+      primary: '#897CEB',
+      secondary: '#B2B7F6',
+      error: '#D65E57',
+    },
     extend: {
+      fontFamily: {
+        display: ['var(--font-montserrat)', ...fontFamily.sans],
+        body: ['var(--font-nunito)', ...fontFamily.sans],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
