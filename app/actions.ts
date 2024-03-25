572db8879f0c2b4f86cb6f9274d8a00bbd88295f
Raaed Kabir <raaed.kabir@gmail.com>
2024-03-24 20:47:10 -0400

    Implement ContactForm logic

--- app/actions.ts
+++ app/actions.ts
@@ -5,6 +5,7 @@ import { cookies } from 'next/headers'
 import { deleteS3Object } from '@/services/storage'
 import { deleteEmbed } from '@/services/embeddings'
 import { deleteFileFromChatHistory } from '@/services/dynamodb'
+import { z } from 'zod'
 
 export async function deleteFile(userId: string, fileName: string) {
   const idToken = cookies().get('IdToken')?.value!
@@ -16,4 +17,43 @@ export async function deleteFile(userId: string, fileName: string) {
   ])
 
   revalidatePath('/dashboard')
+}
+
+export async function sendEmail(
+  _prevState: {
+    message: string
+    status: string
+  },
+  formData: FormData
+) {
+  const schema = z.object({
+    name: z.string().min(1),
+    email: z.string().email(),
+    subject: z.string().min(1),
+    message: z.string().min(1),
+  })
+  const parse = schema.safeParse({
+    name: formData.get('name'),
+    email: formData.get('email'),
+    subject: formData.get('subject'),
+    message: formData.get('message'),
+  })
+
+  if (!parse.success) {
+    return { message: 'Failed to send email', status: 'error' }
+  }
+
+  try {
+    await fetch(process.env.CONTACT_FORM_API_ENDPOINT!, {
+      method: 'POST',
+      headers: {
+        'x-api-key': process.env.CONTACT_FORM_API_KEY!,
+      },
+      body: JSON.stringify(parse.data),
+    })
+
+    return { message: 'Successfully sent email', status: 'success' }
+  } catch (e) {
+    return { message: 'Failed to send email', status: 'error' }
+  }
 }
---
+++ components/ContactForm.tsx
@@ -0,0 +1,137 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { useFormState, useFormStatus } from 'react-dom'
+import { sendEmail } from '@/app/actions'
+
+const initialState = {
+  message: '',
+  status: '',
+}
+
+export default function ContactForm() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [subject, setSubject] = useState('')
+  const [message, setMessage] = useState('')
+  const [state, formAction] = useFormState(sendEmail, initialState)
+  const { pending } = useFormStatus()
+
+  useEffect(() => {
+    if (state?.status === 'success') {
+      setName('')
+      setEmail('')
+      setSubject('')
+      setMessage('')
+    }
+  }, [state?.status])
+
+  return (
+    <form className="space-y-6" action={formAction}>
+      <div>
+        <div className="flex items-center justify-between">
+          <label
+            htmlFor="name"
+            className="sr-only block text-sm font-medium leading-6 text-gray-dark"
+          >
+            Your Name
+          </label>
+        </div>
+        <div className="mt-2">
+          <input
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Your Name"
+            required
+            className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-dark shadow-sm ring-1 ring-inset ring-gray-light placeholder:text-gray focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+
+      <div>
+        <div className="flex items-center justify-between">
+          <label
+            htmlFor="email"
+            className="sr-only block text-sm font-medium leading-6 text-gray-dark"
+          >
+            Email address
+          </label>
+        </div>
+        <div className="mt-2">
+          <input
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="Your Email Address"
+            required
+            className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-dark shadow-sm ring-1 ring-inset ring-gray-light placeholder:text-gray focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+
+      <div>
+        <div className="flex items-center justify-between">
+          <label
+            htmlFor="subject"
+            className="sr-only block text-sm font-medium leading-6 text-gray-dark"
+          >
+            Subject
+          </label>
+        </div>
+        <div className="mt-2">
+          <input
+            id="subject"
+            name="subject"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+            type="text"
+            placeholder="Subject"
+            required
+            className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-dark shadow-sm ring-1 ring-inset ring-gray-light placeholder:text-gray focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+
+      <div>
+        <div className="flex items-center justify-between">
+          <label
+            htmlFor="message"
+            className="sr-only block text-sm font-medium leading-6 text-gray-dark"
+          >
+            Enter your message
+          </label>
+        </div>
+        <div className="mt-2">
+          <textarea
+            id="message"
+            name="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Enter your message"
+            required
+            className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-dark shadow-sm ring-1 ring-inset ring-gray-light placeholder:text-gray focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+          />
+        </div>
+      </div>
+
+      <p aria-live="polite" className="sr-only" role="status">
+        {state?.message}
+      </p>
+
+      <div>
+        <button
+          type="submit"
+          disabled={pending}
+          className="inline-flex h-9 w-full items-center justify-center rounded-md bg-primary px-3 text-sm font-medium text-white transition-colors hover:bg-primary/90 focus:outline-none focus:ring-4 focus:ring-primary/50 disabled:pointer-events-none disabled:opacity-50"
+        >
+          Send Message
+        </button>
+      </div>
+    </form>
+  )
+}
